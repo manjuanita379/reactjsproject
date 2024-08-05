600a30c5613e306bc509b1f800aa8c90535675a8
manjuanita379 <manjuvk379@gmail.com>
2024-08-05 04:47:51 +0000

    first commit

---
+++ my-lambda-project/src/handler.ts
@@ -0,0 +1,11 @@
+// src/handler.ts
+import { APIGatewayProxyHandler } from 'aws-lambda';
+export const hello: APIGatewayProxyHandler = async (event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello from Lambda!',
+      input: event,
+    }),
+  };
+};
---
+++ socketService.js
@@ -0,0 +1,3 @@
+import io from 'socket.io-client';
+const socket = io('http://3.234.255.27:3001'); // Replace with your server URL
+export default socket;
